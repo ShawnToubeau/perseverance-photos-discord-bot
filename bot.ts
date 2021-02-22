8caf6fb1b5feec3b453aa5465698f4dd50308bdb
Shawn Toubeau <shawntoubeau@gmail.com>
2021-02-21 21:27:35 -0500

    feat: Adds helper methods + basic functionality
    - getMissingImages, addOne, deleteMessagesInChannel helper functions
    - able to put all images pulled from URL into channel

--- bot.ts
+++ bot.ts
@@ -1,4 +1,4 @@
-import { Client, TextChannel } from 'discord.js';
+import {Client, Message, TextChannel} from 'discord.js';
 import { config } from "dotenv";
 import {GetRawImages} from "./marsScraper";
 
@@ -9,37 +9,174 @@ const TOKEN = process.env.TOKEN;
 const CHANNEL_ID = process.env.CHANNEL_ID;
 
 client.on('ready', async () => {
-    const channel = client.channels.cache.get(CHANNEL_ID) as TextChannel;
-    // console.log("Channel", channel)
-    // console.log("Channel message", channel.messages)
-    channel.messages.fetch().then(res => {
-        console.log(res)
-    })
+    // getMissingImages()
+    // getMessagesInChannel(CHANNEL_ID)
+    //     .then(msgs => {
+    //         msgs.forEach(msg => console.log("MSG: ", msg))
+    //     })
+
+
     // console.log("Cache", channel.messages.cache)
     // console.log("?", channel.messages.cache.entries())
-    const sentImages = new Set();
+    // const sentImages = new Set();
     // retrieve messages from the channel
     // channel.messages.cache.forEach(msg => {
     //     console.log(msg);
     // })
 });
 
+const getChannel = (): TextChannel => {
+    return client.channels.cache.get(CHANNEL_ID) as TextChannel;
+}
+
+/**
+ * getMessagesInChannel retrieves all messages within a specific channel.
+ *
+ * @returns {Promise<Message[]>} - channel messages
+ */
+const getMessagesInChannel = async (): Promise<Message[]> => {
+    return new Promise(((resolve, reject) => {
+        getChannel().messages.fetch()
+            .then(res => {
+                resolve(res.array());
+            })
+            .catch(e => {
+                reject(`error fetching channel messages: ${e}`)
+            })
+    }))
+}
+
+/**
+ * deleteMessagesInChannel deletes all messages from a channel.
+ *
+ * @returns {Promise<string>} - message deletion result
+ */
+const deleteMessagesInChannel = async (): Promise<string> => {
+    return new Promise(((resolve, reject) => {
+        console.log("deleting images in channel..")
+        getMessagesInChannel().then((messages) => {
+            const deleteReqs: Promise<Message>[] = [];
+
+            messages.forEach(msg => {
+                deleteReqs.push(msg.delete())
+            })
+
+            Promise.all(deleteReqs)
+                .then(delRes => {
+                    resolve(`number of messages deleted: ${delRes.length}`);
+                }).catch(err => {
+                    reject(`error deleting messages in channel: ${err}`)
+            })
+        })
+    }))
+}
+
+/**
+ * getOne adds one image to a channel.
+ */
+const addOne = () => {
+    GetRawImages().then(srcs => {
+        if (srcs.length > 0) {
+            const first = srcs[1];
+
+            getChannel().send({files: [first]})
+                .then(() => {
+                    console.log("one image added to channel")
+                })
+                .catch(console.error)
+        }
+    })
+}
+
+const getImagesNamesInChannel = (): Promise<string[]> => {
+    return new Promise<string[]>((resolve, reject) => {
+        getMessagesInChannel()
+            .then(messages => {
+                let images: string[] = [];
+
+                messages.forEach(msg => {
+                    if (msg.author.bot) {
+                        msg.attachments.forEach(img => {
+                            images.push(img.name)
+                        })
+                    }
+                })
+
+                resolve(images)
+            })
+            .catch(reject)
+    })
+}
+
+const getMissingImages = (): Promise<string> => {
+    return new Promise<string>(((resolve, reject) => {
+        GetRawImages()
+            .then(sources => {
+                // console.log("Srcs", sources)
+
+                if (sources.length > 0) {
+                    getImagesNamesInChannel()
+                        .then(presentImages => {
+                            // console.log("Messages", images)
+                            const groupGroups: string[][] = []
+                            let imgGroups: string[] = []
+                            const channel = getChannel();
+
+                            sources.forEach((src, idx) => {
+                                if (!presentImages.some(presentImage => src.includes(presentImage))) {
+                                    if (idx !== 0 && idx % 10 === 0) {
+                                        groupGroups.push(imgGroups)
+                                        imgGroups = []
+                                    }
+
+                                    imgGroups.push(src)
+                                }
+                            })
+
+                            // get last remaining images
+                            if (imgGroups.length > 0) {
+                                groupGroups.push(imgGroups)
+                            }
+
+                            const addReqs: Promise<Message>[] = []
+
+                            groupGroups.forEach(group => {
+                                addReqs.push(channel.send({files: group}))
+                            })
+
+                            console.log(`adding ${groupGroups.reduce((count, row) => count + row.length, 0)} images..`)
+                            Promise.all(addReqs)
+                                .then(res => {
+                                    console.log(res)
+                                    resolve(`${res.reduce((count, row) => count + row.attachments.array().length, 0)} images added to channel`)
+                                })
+                                .catch(e => {
+                                    reject(`error adding images to channel: ${e}`)
+                                })
+                        })
+                        .catch(reject)
+                }
+            })
+            .catch(reject)
+    }))
+}
+
 client.on('message', msg => {
+    if (msg.content === "clear") {
+        deleteMessagesInChannel()
+            .then(console.log)
+            .catch(console.error)
+    }
 
+    if (msg.content === "add1") {
+        addOne();
+    }
 
-
-
-    // if (msg.content === 'ping') {
-    //     GetRawImages().then(srcs => {
-    //         console.log("Sources", srcs)
-    //
-    //         if (srcs.length > 0) {
-    //             const first = srcs[1];
-    //
-    //             channel.send({files: [first]}).then();
-    //         }
-    //     })
-    // }
+    if (msg.content === "missing") {
+        getMissingImages()
+            .then(console.log)
+            .catch(console.error)
+    }
 });
 
 client.login(TOKEN).then(() => {
--- marsScraper.ts
+++ marsScraper.ts
@@ -1,4 +1,4 @@
-import { By, Builder } from 'selenium-webdriver';
+import { By, Builder, Key } from 'selenium-webdriver';
 import { config } from "dotenv";
 config();
 
@@ -11,19 +11,39 @@ const driver = new Builder()
 driver.get(url).then();
 
 export const GetRawImages = (): Promise<string[]> => {
-    return new Promise(async (resolve) => {
+    return new Promise(async (resolve, reject) => {
         let sources: string[] = []
 
-        await driver.findElement(By.className("raw_images_list")).then( async (imgList) => {
-            await imgList.findElements(By.css("img")).then( async (images) => {
-                for (const img of images) {
-                    await img.getAttribute("src")
-                        .then(src => {
-                            sources.push(src);
-                        });
-                }
+        await driver.findElement(By.id("header_pagination"))
+            .then( async (input) => {
+                await input.getAttribute("max")
+                    .then( async (pageMax) => {
+                        // loop through all the pages
+                        for (let i = 0; i < +pageMax; i++) {
+                            // get images on page
+                            await driver.findElement(By.className("raw_images_list")).then( async (imgList) => {
+                                await imgList.findElements(By.css("img")).then( async (images) => {
+                                    for (const img of images) {
+                                        await img.getAttribute("src")
+                                            .then(src => {
+                                                sources.push(src);
+                                            });
+                                    }
+
+                                    // change to next page
+                                    // await input.sendKeys(Key.ARROW_UP)
+                                })
+                            })
+                        }
+                    })
+                    .catch(err => {
+                        reject(`error getting pagination input max: ${err}`)
+                    })
+
             })
-        })
+            .catch(err => reject(`error getting pagination input: ${err}`))
+
+        console.log("Num sources: ", sources.length)
 
         resolve(sources)
     })
